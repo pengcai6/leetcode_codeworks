d4c30c0683f25422842b8caa25fa1074838bcd3a
pengcai6 <pengcai6@gmail.com>
2025-08-26 20:52:56 +0800

    update: 3

--- src/leetcode/editor/cn/mycode/ID3/无重复字符的最长子串.java
+++ src/leetcode/editor/cn/mycode/ID3/无重复字符的最长子串.java
@@ -1,29 +1,24 @@
 package leetcode.editor.cn.mycode.ID3;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class 无重复字符的最长子串 {
     class Solution {
         public int lengthOfLongestSubstring(String s) {
-            int n = s.length();
-            if (n == 1) return 1;
-            if (n == 0) return 0;
-            int l = 0, r = 1;
-            int res = 0;
-            while (r < n) {
-                boolean foundDuplicate = false;
-                for (int k = l; k < r; k++) {
-                    if (s.charAt(k) == s.charAt(r)) {
-                        res = Math.max(res, r - l); // 更新最大长度
-                        l = k + 1; // 更新左指针
-                        foundDuplicate = true;
-                        break;
-                    }
+            int n = s.length(),maxLen=0;
+            Map<Character,Integer> map=new HashMap<>();
+            for (int i = 0,j=0; j <n; j++) {
+                if(map.containsKey(s.charAt(j)))
+                {   //找到上一次出现的位置，并且更新
+                    i=Math.max(map.get(s.charAt(j)),i);
                 }
-                if (!foundDuplicate) {
-                    res = Math.max(res, r - l + 1); // 如果没有重复字符，更新最大长
-                }
-                r++;
+                //计算当前到上一个相同的出现位置的距离
+                maxLen=Math.max(maxLen,j-i+1);
+                //更新当前出现的位置
+                map.put(s.charAt(j),j+1);
             }
-            return res;
+            return maxLen;
         }
     }
 
