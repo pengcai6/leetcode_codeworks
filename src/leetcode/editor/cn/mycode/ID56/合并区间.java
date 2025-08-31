acc714387d3c2170feaa3df08fe9d216915f094b
pengcai6 <pengcai6@gmail.com>
2025-08-31 23:47:08 +0800

    update: 56

--- src/leetcode/editor/cn/mycode/ID56/合并区间.java
+++ src/leetcode/editor/cn/mycode/ID56/合并区间.java
@@ -16,11 +16,13 @@ public class 合并区间 {
                 int[] curr = intervals[i];
                 // 获取list中最后一个区间
                 int[] last = list.get(list.size() - 1);
+                //判断当前的左边界是否小于列表最后一个的右边界，小于的话，说明重叠，更新列表的右边界
                 if(curr[0]<=last[1])
                 {
                     //有重叠
                     last[1]=Math.max(curr[1],last[1]);
                 }
+                //否则将当前添加到列表中
                 else {
                     list.add(curr);
                 }
